3420884f3423e3052ffd57ad75728ddb8353a1c2
NPRojas <63315208+NPRojas@users.noreply.github.com>
2024-04-06 20:45:24 -0400

    create method the declares the winner

--- RockPaperScissors/src/Main.java
+++ RockPaperScissors/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Main {
     public static String createRandomChoice() {
         // stores a random number from 1 to 3
@@ -19,7 +21,30 @@ public class Main {
         return move;
     }
 
-    public static void main(String[] args) {
+    public static String declareWinner(String player1, String player2) {
+        String winner;
 
+        if (player1.equals(player2)) {
+            winner = "It's a tie!";
+        } else if (player1.equals("paper") && player2.equals("rock") ||
+                   player1.equals("rock") && player2.equals("scissors") ||
+                   player1.equals("scissors") && player2.equals("paper")) {
+            winner = "Player 1 Wins!";
+
+        } else {
+            winner = "Player 2 Wins!";
+        }
+
+        return winner;
+    }
+
+    public static void main(String[] args) {
+        // sets a move for player 1
+        String player1 = createRandomChoice();
+        // sets a move for player 2
+        String player2 = createRandomChoice();
+
+        System.out.println("Player 1 chooses " + player1);
+        System.out.println("Player 2 chooses " + player2);
     }
 }
